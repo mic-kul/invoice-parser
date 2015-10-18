586116b550eafcdcfa80aafa1cf9d108b652c94b
Michał Kulesza <michal.kulesza@netguru.co>
2015-10-18 21:50:11 +0200

    Extend base parser to allow different dates and expense type

--- lib/parser/base_parser.rb
+++ lib/parser/base_parser.rb
@@ -4,5 +4,25 @@ module Parser
     def initialize(text)
       @text = text
     end
+
+    def purchase_date
+      date
+    end
+
+    def pay_until
+      date
+    end
+
+    def receive_date
+      date
+    end
+
+    def date
+      nil
+    end
+
+    def expense_type
+      :other
+    end
   end
 end
